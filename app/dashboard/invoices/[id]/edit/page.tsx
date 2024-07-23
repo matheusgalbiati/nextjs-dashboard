2374d6967e53aa0da8c805a492325aa288afb065
Matheus Galbiati <matheusgalbiati00@gmail.com>
2024-07-23 11:58:41 -0300

    add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,14 @@
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-export default async function Page({ params }: {params: { id: string }}) {
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
+
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
